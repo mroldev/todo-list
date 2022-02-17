409b1a495511dd3571f477578e68358bef96f5c2
mroldev <mrolpierre@gmail.com>
2022-02-17 17:33:35 +0100

    Todo start

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+   <div>
+          <todos> </todos>
+   </div>
+</template>
+
+<script>
+import todos from './components/todos.vue'
+
+export default {
+  name: 'App',
+  components: {
+    todos
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/todos.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <section class="todoapp">
+            <header class="header">
+                <h1>Todos</h1>
+                <input type="text" class="new-todo" placeholder="Ajouter une tache" v-model="newTodo" @keyup.enter="addTodo">
+            </header>
+
+            <div class="main">
+                <ul class="todo-list">
+                    <li class="todo" v-for="todo in todos" :key="todo" >
+                        <div class="view">
+                            <input type="checkbox" v-model="computed">
+                            <label>{{todo.name}}</label>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
+        </section>
+    </div>
+</template> 
+
+
+<script>
+export default {
+
+    data(){
+        return{
+             todos : [],
+            newTodo: ''
+        }
+    },
+
+    methods: {
+        addTodo(){
+            this.todos.push({
+                completed : false,
+                name : this.newTodo
+            })
+            this.newTodo = ''
+        }
+    }
+    
+}
+</script>
+
+
+<style  src="./todo.css">
+    
+</style>
