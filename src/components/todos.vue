e511a058a35110f5614b7513779998612c0c710d
mroldev <mrolpierre@gmail.com>
2022-02-21 19:23:11 +0100

    first commit

--- src/components/todos.vue
+++ src/components/todos.vue
@@ -7,16 +7,30 @@
             </header>
 
             <div class="main">
+                <input type="checkbox" class=""  v-model="allDone">
                 <ul class="todo-list">
-                    <li class="todo" v-for="todo in todos" :key="todo" >
+                    <li class="todo" v-for="todo in  filtersTodos" :key="todo" :class="{completed: todo.completed, editing: todo=== editing}" >
                         <div class="view">
-                            <input type="checkbox" v-model="computed">
-                            <label>{{todo.name}}</label>
+                            <input type="checkbox" v-model="todo.completed" class="toggle">
+                            <label @dblclick="editTodo(todo)" >{{todo.name}}</label>
+                            <button class="destroy" @click.prevent="deleteTodo(todo)"></button>
                         </div>
+                         <input type="text" class="edit" v-model="todo.name" @keyup.enter="doneEdit" @keyup.esc ="cancelEdit "  v-focus="todo===editing" @blur="doneEdit"> 
                     </li>
                 </ul>
             </div>
+                <footer class="footer" v-show="hasTodos">
+                        <span class="todo-count"><strong>{{remaining}}</strong> Taches à faires </span>
 
+                        <ul class="filters">
+                            <li><a href="#" :class="{selected : filter === 'all'}" @click.prevent="filter ='all'">tout</a></li>
+                            <li><a href="#" :class="{selected : filter === 'todo'}" @click.prevent="filter ='todo'">A faire</a></li>
+                            <li><a href="#" :class="{selected : filter === 'done'}" @click.prevent="filter ='done'">Faites</a></li>
+                        </ul>
+
+                        <button class="clear-completed" v-show="completed" @click.prevent="deleteCompleted">Supprimer les taches termine</button>
+
+                </footer>
         </section>
     </div>
 </template> 
@@ -24,23 +38,95 @@
 
 <script>
 export default {
+   
 
     data(){
         return{
              todos : [],
-            newTodo: ''
+            newTodo: '',
+            filter : '',
+            editing : null,
+            oldTodo : ''
         }
     },
 
     methods: {
+
         addTodo(){
             this.todos.push({
                 completed : false,
                 name : this.newTodo
             })
             this.newTodo = ''
+        },
+
+        deleteTodo(todo){
+            this.todos = this.todos.filter(i => i !== todo)
+        },
+
+        deleteCompleted(){
+            this.todos = this.todos.filter(todo=> !todo.completed)
+        },
+        editTodo(todo){
+                this.editing = todo,
+                this.oldTodo = todo.name
+        },
+        doneEdit(){
+             this.editing = null 
+        },
+
+        cancelEdit(){
+            this.editing.name = this.oldTodo
+            this.doneEdit()
+
+        }
+    },
+
+        
+
+    computed:{
+            allDone : {
+                get(){
+                    return this.remaining === 0
+                },
+                set(value){
+                    this.todos.forEach(todo=> todo.completed = value)
+                }
+            },
+
+        remaining (){
+            return this.todos.filter(todo=>!todo.completed).length
+        },
+
+        completed (){
+            return this.todos.filter(todo=>todo.completed).length
+        },
+        
+        hasTodos(){
+            return this.todos.length > 0
+        },
+
+        filtersTodos(){
+            if(this.filter === 'todo'){
+                return this.todos.filter(todo=>!todo.completed)
+            }else if(this.filter ==='done'){
+                return this.todos.filter(todo=> todo.completed)
+            }
+            return this.todos
+        },
+
+    },
+
+    directives : {
+        focus(el,value){
+            if(value){       
+                     el.focus()    
+            }
         }
     }
+  
+  
+    
     
 }
 </script>
